1599d0d8afa361c0cd9307f962f81e6eaed149ae
syedanusrat007 <syedanusrat007@gmail.com>
2021-05-08 16:11:27 +0600

    node js

---
+++ wordtranslator/src/components/TranslateForm.vue
@@ -0,0 +1,53 @@
+
+
+
+
+<template>
+  <div class="row  " id="translateForm">
+    <div class="col-md-6 col-md-offset-3">
+      <form id="transForm" class=" well " v-on:submit="formSubmit">
+        <input class="form-control" type="text" v-model="textToTranslate" placeholder="Enter a Word...">
+        <br>
+        <select class="form-control" v-model="language">
+          <option value="bn">Bengali</option>
+          <option value="de">German</option>  
+          <option value="ru">Russian</option>
+          <option value="es">Spanish</option>
+          <option value="fr">French</option>
+          <option value="zh">Chinese</option>
+        </select>
+        <br>
+        <input class="btn btn-primary" type="submit" value="Translate">
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'translateForm',
+  data(){
+    return {
+      textToTranslate:'',
+      language:''
+    }
+  },
+  created() {
+    this.language = 'bn';
+  },
+  methods: {
+    formSubmit(e){
+      this.$emit('formSubmit', this.textToTranslate, this.language);
+      e.preventDefault();
+    }
+  }
+}
+</script>
+
+<style>
+  #transForm{
+    border-radius:10px;
+    border: 1px #ccc solid;
+  
+  }
+</style>
